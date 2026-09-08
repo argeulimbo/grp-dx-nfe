3883fdbf9169e41882407015d66b27eaf5aeb354
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-08 11:47:31 -0300

    feat: add structure on documentos.component.html / .ts

--- src/app/pages/documentos/documentos.component.ts
+++ src/app/pages/documentos/documentos.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { DxDataGridComponent } from 'devextreme-angular';
+import { Component, OnInit } from '@angular/core';
+import { DxDataGridComponent, DxButtonComponent } from 'devextreme-angular';
 
 @Component({
   imports: [
-    DxDataGridComponent
-  ],
+    DxDataGridComponent,
+    DxButtonComponent
+],
   selector: 'app-documentos.component',
   styleUrl: './documentos.component.scss',
   templateUrl: './documentos.component.html',
 })
 export class DocumentosComponent {
+
+  constructor() {
+
+  }
+
+  createDocumento() {
+    console.log('Dx-Button NOVO acionado')
+  }
+
+
+
 }
