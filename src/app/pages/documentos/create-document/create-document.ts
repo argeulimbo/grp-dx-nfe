2e67d24aa0a29d9d9e800b4b39b250dedab20260
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-08 13:31:55 -0300

    feat: add pages/documentos/create && edit component page

---
+++ src/app/pages/documentos/create-document/create-document.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-create-document',
+  styleUrl: './create-document.scss',
+  templateUrl: './create-document.html',
+})
+export class CreateDocument {}
---
+++ src/app/pages/documentos/edit-document.component/edit-document.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-edit-document.component',
+  styleUrl: './edit-document.component.scss',
+  templateUrl: './edit-document.component.html',
+})
+export class EditDocumentComponent {}
