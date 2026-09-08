c5131f3ba641788137d4a906abd9ee1d5fd67a3c
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-08 16:49:19 -0300

    feat: add service documento.ts

--- src/app/pages/documentos/documentos.component.ts
+++ src/app/pages/documentos/documentos.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { DxDataGridComponent, DxButtonComponent } from 'devextreme-angular';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { DxDataGridComponent, DxButtonComponent, DxTextBoxComponent } from 'devextreme-angular';
+
+import { NotaFiscal } from './documentos';
 
 @Component({
   imports: [
     DxDataGridComponent,
-    DxButtonComponent
+    DxButtonComponent,
+    DxTextBoxComponent
 ],
   selector: 'app-documentos.component',
   styleUrl: './documentos.component.scss',
   templateUrl: './documentos.component.html',
 })
-export class DocumentosComponent {
+export class DocumentosComponent implements OnInit {
 
-  constructor() {
+  documentos: NotaFiscal[] = [];
 
+  filtroPorNumero: string = '';
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.changeDetectorRef.detectChanges();
   }
 
   createDocumento() {
---
+++ src/app/shared/services/documento.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentoService {
+
+  // Endpoint NOTAS
+  private readonly API = 'http://localhost:8080/notas';
+
+
+}
