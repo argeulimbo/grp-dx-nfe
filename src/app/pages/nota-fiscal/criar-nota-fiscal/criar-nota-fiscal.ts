b26f8fc8b61e54cb935be1fe7285cb295afc43f4
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-09 23:26:31 -0300

    feat: add pages/nota-fiscal/criar-nota-fiscal

---
+++ src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-criar-nota-fiscal',
+  styleUrl: './criar-nota-fiscal.scss',
+  templateUrl: './criar-nota-fiscal.html',
+})
+export class CriarNotaFiscal {}
