a9f78eb897aaacbc61f0c0077980f9d184f95c8b
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-11 19:20:55 -0300

    feat: add criar-nota-fiscal.html form

--- src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
+++ src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
@@ -1,14 +1,19 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
+import { DxFormModule } from 'devextreme-angular';
+import { NotaFiscal } from '../../documentos/documentos';
+
 @Component({
   imports: [
-
+    DxFormModule,
   ],
   selector: 'app-criar-nota-fiscal',
   styleUrl: './criar-nota-fiscal.scss',
   templateUrl: './criar-nota-fiscal.html',
 })
 export class CriarNotaFiscalComponent implements OnInit {
+
+  notas: NotaFiscal[] = [];
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef
