be155c90eb658779cb98b0632ab70a4eb8563683
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-14 09:14:49 -0300

    feat: add variable dataEmissao

--- src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
+++ src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
@@ -14,6 +14,7 @@ import { NotaFiscal } from '../../documentos/documentos';
 export class CriarNotaFiscalComponent implements OnInit {
 
   notas: NotaFiscal[] = [];
+  dataEmissao: Date = new Date();
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef
