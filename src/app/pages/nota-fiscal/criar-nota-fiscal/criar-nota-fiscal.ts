03cacc5700cdc3df467480270ce43b0dccbd997b
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-14 22:59:59 -0300

    feat: fim tela(view) de criar nota fiscal

--- src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
+++ src/app/pages/nota-fiscal/criar-nota-fiscal/criar-nota-fiscal.ts
@@ -1,26 +1,70 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
-import { DxFormModule, DxSelectBoxComponent } from 'devextreme-angular';
-import { Cliente, NotaFiscal } from '../../documentos/documentos';
+import { DxButtonComponent, DxDataGridComponent, DxFormModule, DxSelectBoxComponent } from 'devextreme-angular';
+import { DxiColumnComponent, DxoDropDownOptionsComponent, DxoEditingComponent, DxoLookupComponent } from 'devextreme-angular/ui/nested';
+import { DxiToolbarItemComponent } from 'devextreme-angular/ui/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [DxFormModule, DxSelectBoxComponent],
+  imports: [
+    DxFormModule,
+    DxSelectBoxComponent,
+    DxoDropDownOptionsComponent,
+    DxiToolbarItemComponent,
+    DxDataGridComponent,
+    DxoEditingComponent,
+    DxiColumnComponent,
+    DxoLookupComponent,
+    DxButtonComponent,
+    RouterLink,
+  ],
   selector: 'app-criar-nota-fiscal',
   styleUrl: './criar-nota-fiscal.scss',
   templateUrl: './criar-nota-fiscal.html',
 })
 export class CriarNotaFiscalComponent implements OnInit {
-  nota: NotaFiscal = {
+  nota: any = {
+    numero: null,
+    codigoCliente: null,
     dataEmissao: new Date(),
+    valorTotal: 0,
+    itens: [],
   };
 
-  clientes: Cliente[] = [];
-
-  dataEmissao: Date = new Date();
+  clientes: any[] = [];
+  produtos: any[] = [];
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.changeDetectorRef.detectChanges();
+  }
+
+  btnNovoClienteOptions = {
+    text: '+ Cadastrar Novo Cliente',
+    type: 'default',
+    onClick: () => this.abrirModalOuNavegarCriarCliente(),
+  };
+
+  // GET all clientes
+  carregarClientes() {
+    return null;
+  }
+
+  // GET all produtos
+  carregarProdutos() {
+    return null;
+  }
+
+  abrirModalOuNavegarCriarCliente() {
+    return null;
+  }
+
+  atualizarValorTotal() {
+    return null;
+  }
+
+  salvarNota() {
+    return null;
   }
 }
