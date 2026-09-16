0e4112c477fd7cd3ee39abcc2896c583fe3fcba3
Argeu Souza <argeusousaptbr@gmail.com>
2026-09-16 16:31:34 -0300

    feat: add produtos page and produtos service

---
+++ src/app/pages/produtos/criar-produto/criar-produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-criar-produto',
+  styleUrl: './criar-produto.scss',
+  templateUrl: './criar-produto.html',
+})
+export class CriarProduto {}
---
+++ src/app/pages/produtos/lista-produto.component/lista-produto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-lista-produto.component',
+  styleUrl: './lista-produto.component.scss',
+  templateUrl: './lista-produto.component.html',
+})
+export class ListaProdutoComponent {}
